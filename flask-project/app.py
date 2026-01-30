da8568259b3ded874f5c2f40d40c7ec2c094d13d
Krrish166 <bijayapatra133@gmail.com>
2026-01-30 13:44:29 +0530

    flask-project/

--- flask-project/app.py
+++ flask-project/app.py
@@ -1,31 +1,20 @@
-from flask import Flask, jsonify
-from config import Config
+from flask import Flask
 
 app = Flask(__name__)
-app.config.from_object(Config)
 
 @app.route("/")
 def home():
-    return jsonify({
-        "message": "Hello from Python Flask App 🚀",
-        "env": app.config["ENV_NAME"]
-    })
-
-@app.route("/health")
-def health():
-    return jsonify({"status": "UP"}), 200
-
-@app.route("/api/books")
-def books():
-    return jsonify([
-        {"id": 1, "title": "Terraform Basics"},
-        {"id": 2, "title": "AWS for DevOps"},
-        {"id": 3, "title": "Python Automation"}
-    ])
+    return """
+    <html>
+        <head>
+            <title>My App</title>
+        </head>
+        <body style="font-family: Arial; background-color: white;">
+            <h1>🚀 Deployment Successful</h1>
+            <p>Deployed via GitHub Actions + Terraform</p>
+        </body>
+    </html>
+    """
 
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=8000,
-        debug=True
-    )
+    app.run(host="0.0.0.0", port=8000)
