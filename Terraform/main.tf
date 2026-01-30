2b094313aeaef12cadba1d0194e15c231d86425c
Krrish166 <bijayapatra133@gmail.com>
2026-01-30 13:55:20 +0530

    Terraform/

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_security_group" "flask_sg" {
-  name = "flask-app-sg"
+  name = "flask-app"
 
   ingress {
     from_port   = 8000
@@ -31,34 +31,45 @@ resource "aws_instance" "flask_ec2" {
   ami           = "ami-0ff5003538b60d5ec" # Amazon Linux 2023 (Mumbai)
   instance_type = "t3.micro"
 
-
   vpc_security_group_ids = [aws_security_group.flask_sg.id]
 
   user_data = <<-EOF
               #!/bin/bash
               dnf update -y
-              dnf install -y python3 pip git
+              dnf install -y python3 python3-pip
 
               mkdir -p /opt/flask-app
               cd /opt/flask-app
 
-              cat <<EOT > app.py
-              from flask import Flask, jsonify
+              cat <<'EOT' > app.py
+              from flask import Flask
+
               app = Flask(__name__)
 
               @app.route("/")
               def home():
-                  return jsonify({"message": "Deployed via GitHub Actions + Terraform 🚀"})
+                  return """
+                  <html>
+                    <head>
+                      <title>Flask App</title>
+                    </head>
+                    <body style="font-family: Arial; background-color: white;">
+                      <h1>🚀 Deployment Successful</h1>
+                      <p>Deployed via GitHub Actions + Terraform</p>
+                    </body>
+                  </html>
+                  """
 
-              app.run(host="0.0.0.0", port=8000)
+              if __name__ == "__main__":
+                  app.run(host="0.0.0.0", port=8000)
               EOT
 
               pip3 install flask
-              nohup python3 app.py &
+              nohup python3 app.py > /var/log/flask.log 2>&1 &
               EOF
 
   tags = {
-    Name = "flask-github-actions"
+    Name = "flask-app-github-actions"
   }
 }
 
